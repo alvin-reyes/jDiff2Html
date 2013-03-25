3822b7cce51a8cf0faa2d7b5233554459b42225b
Karl Chu <karl.chu@team.telstra.com>
2013-03-26 10:18:27 +1100

    Changed velocity engine to use classpath resource loader Moved test jars into test scope to win Biggest Loser Moved .vm files into resource tree

--- src/main/java/jDiff2Html/VelocityRenderer.java
+++ src/main/java/jDiff2Html/VelocityRenderer.java
@@ -3,19 +3,21 @@ package jDiff2Html;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 import java.io.StringWriter;
 
 public class VelocityRenderer {
-    public static final String TEMPLATE_PATH = "src/main/templates";
+    public static final String TEMPLATE_PATH = "templates/";
 
     public String renderTemplate(String templateName, VelocityContext context) {
         VelocityEngine ve = new VelocityEngine();
-        ve.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH, TEMPLATE_PATH);
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
         ve.init();
 
-        Template t = ve.getTemplate(templateName);
-
+        Template t = ve.getTemplate(TEMPLATE_PATH + templateName);
         final StringWriter writer = new StringWriter();
         t.merge(context, writer);
 
